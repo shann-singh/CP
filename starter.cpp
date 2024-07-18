ef1a23d9589f07e562d00b26c895673a15ab74b4
Shantanu Singh <shann.singh@gmail.com>
2024-07-18 17:45:18 +0530

    restructure

---
+++ starter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <ios>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin);   
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    int t;
+    cin >> t;
+    while(t--) {
+        cout << t << endl;
+    }
+}
